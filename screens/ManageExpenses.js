ca6e0e210599647e3cc2e6b412953e93472c13f3
Matthew Alvarez <you@example.com>
2022-08-02 18:02:01 -0400

    modal and screen options

--- screens/ManageExpenses.js
+++ screens/ManageExpenses.js
@@ -1,6 +1,16 @@
 import { Text } from "react-native";
+import { useLayoutEffect } from "react";
 
-function ManageExpenses() {
+function ManageExpenses({ route, navigation }) {
+  const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+
   return <Text>Manage expense screen</Text>;
 }
 
